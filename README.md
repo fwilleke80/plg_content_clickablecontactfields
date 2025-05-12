c2b288f7c6e1be0ec8a2da078759c7f2c34f2572
Frank Willeke <github@frankwilleke.de>
2025-05-12 12:01:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Fields plugin "Clickable contact fields"
+
+## What is it?
+
+This plugin makes phone and SMS numbers, as well as usernames and numbers for WhatsApp, Telegram, and Signal clickable when displaying them from custom user fields.
+
+## Usage
+
+In the plugin's settings, define the names of the custom user fields that should be processed. The names listed in these settings should be used as names for the custom user pforile fields.
+
+## License
+
+Published under GNU Public License 2 (see LICENSE.txt).
---
+++ clickablecontactfields.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fields.clickablecontactfields
+ * @version     1.0.6
+ * @author      Frank Willeke
+ * @license     GNU/GPL
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Log\Log;
+
+/**
+ * @brief Make contact fields clickable by overriding their rendered value.
+ */
+class PlgFieldsClickablecontactfields extends CMSPlugin
+{
+	/**
+	 * @brief After a field is prepared, replace its output with clickable HTML.
+	 *
+	 * @param   string   $context  The display context (e.g. 'com_users.profile')
+	 * @param   object   $item     The item (e.g. the user object)
+	 * @param   object   $field    The Field definition
+	 * @param   mixed    &$value   The field’s rendered value (passed by reference)
+	 *
+	 * @return  void
+	 */
+	public function onCustomFieldsAfterPrepareField(string $context, $item, $field, &$value): void
+	{
+		// No empty values
+		if (trim($value) === '')
+		{
+			return;
+		}
+
+		$name           = strtolower($field->name);
+		$phoneFields    = array_map('trim', explode(',', strtolower($this->params->get('phone_fields',    ''))));
+		$smsFields      = array_map('trim', explode(',', strtolower($this->params->get('sms_fields',    ''))));
+		$smsFields      = array_map('trim', explode(',', strtolower($this->params->get('sms_fields',    ''))));
+		$waFields       = array_map('trim', explode(',', strtolower($this->params->get('whatsapp_fields', ''))));
+		$tgFields       = array_map('trim', explode(',', strtolower($this->params->get('telegram_fields',''))));
+		$signalFields   = array_map('trim', explode(',', strtolower($this->params->get('signal_fields',   ''))));
+		$sanitizeDigits = fn(string $v): string => preg_replace('/\D+/', '', $v);
+
+		// Phone
+		if (\in_array($name, $phoneFields, true))
+		{
+			Log::add('onCustomFieldsAfterPrepareField: '.$field->name.' = '.$value, Log::DEBUG, 'plg_fields_clickablecontactfields');
+			$num   = $sanitizeDigits($value);
+			$value = '<a href="tel:'.$num.'">'.htmlspecialchars($value).'</a>';
+		}
+		// SMS
+		elseif (\in_array($name, $smsFields, true))
+		{
+			Log::add('onCustomFieldsAfterPrepareField: '.$field->name.' = '.$value, Log::DEBUG, 'plg_fields_clickablecontactfields');
+			$num   = $sanitizeDigits($value);
+			$value = '<a href="sms:'.$num.'">'.htmlspecialchars($value).'</a>';
+		}
+		// WhatsApp
+		elseif (\in_array($name, $waFields, true))
+		{
+			Log::add('onCustomFieldsAfterPrepareField: '.$field->name.' = '.$value, Log::DEBUG, 'plg_fields_clickablecontactfields');
+			$num   = $sanitizeDigits($value);
+			$value = '<a href="https://wa.me/'.$num.'" target="_blank" rel="noopener">'.htmlspecialchars($value).'</a>';
+		}
+		// Telegram
+		elseif (\in_array($name, $tgFields, true))
+		{
+			Log::add('onCustomFieldsAfterPrepareField: '.$field->name.' = '.$value, Log::DEBUG, 'plg_fields_clickablecontactfields');
+			$p     = trim($value);
+			$link  = preg_match('/^\+?\d+$/', $p)
+				   ? 'https://t.me/+'.$sanitizeDigits($p)
+				   : 'https://t.me/'.urlencode(ltrim($p, '@'));
+			$value = '<a href="'.$link.'" target="_blank" rel="noopener">'.htmlspecialchars($p).'</a>';
+		}
+		// Signal
+		elseif (\in_array($name, $signalFields, true))
+		{
+			Log::add('onCustomFieldsAfterPrepareField: '.$field->name.' = '.$value, Log::DEBUG, 'plg_fields_clickablecontactfields');
+			$num   = $sanitizeDigits($value);
+			$link  = 'https://signal.me/#p/+' . $num;
+			$value = '<a href="'.$link.'" target="_blank" rel="noopener">'.htmlspecialchars($value).'</a>';
+		}
+	}
+}
